9746a347c41f60eca139e71867e1158d76df70bf
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-07-31 20:44:56 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,8 +4,4 @@ const { Router } = require('@layer0/core/router')
 const { nextRoutes, renderNextPage } = require('@layer0/next')
 
 module.exports = new Router()
-  // Fix while the team resolves the main issue
-  .get('/sitemap.xml', (res) => {
-    renderNextPage('/sitemap.xml', res)
-  })
   .use(nextRoutes) // automatically adds routes for all files under /pages
