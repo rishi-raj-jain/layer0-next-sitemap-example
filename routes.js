de004429fc1c24cbc7b1b6e66df9da08930b7764
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-09-01 11:50:55 +0530

    Use next<12.2 Layer0 routes for client side map

--- routes.js
+++ routes.js
@@ -4,8 +4,10 @@ const { Router } = require('@layer0/core/router')
 const { nextRoutes, renderNextPage } = require('@layer0/next')
 
 module.exports = new Router()
-  // Fix while the team resolves the main issue
-  .get('/sitemap.xml', (res) => {
-    renderNextPage('/sitemap.xml', res)
-  })
-  .use(nextRoutes) // automatically adds routes for all files under /pages
+    .get('/sitemap', ({ redirect }) => {
+        redirect('/sitemap.xml', 301)
+    })
+    .get('/sitemap.xml', (res) => {
+        renderNextPage('/sitemap', res)
+    })
+    .use(nextRoutes) // automatically adds routes for all files under /pages
